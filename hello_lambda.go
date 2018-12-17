6b12dba17aaafbe04c987c9d505fe95889f8fae9
Adam Chasen <adam@chasen.name>
2018-12-17 12:00:26 -0500

    rework for jwt check

--- hello_lambda.go
+++ hello_lambda.go
@@ -9,6 +9,9 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/tidwall/gjson"
+
+        "time"
+        "github.com/dgrijalva/jwt-go"
 )
 
 func main() {
@@ -17,24 +20,52 @@ func main() {
 
 func Handler(ctx context.Context, evt json.RawMessage) (events.APIGatewayProxyResponse, error) {
 
-	param := gjson.Get(string(evt), "param1").String()
-	token := gjson.Get(string(evt), "token")
+	signingKey := os.Getenv("SECRET")
+
+	passedToken := gjson.Get(string(evt), "jwt").String()
+	secret := gjson.Get(string(evt), "token")
 
 	returnVal := events.APIGatewayProxyResponse{
 		Body:       "",
 		StatusCode: 200,
 	}
 
-	if token.String() != os.Getenv("SECRET_KEY_TO_CALL_THIS_LAMBDA") {
-		returnVal.Body = "Error - wrong token"
-		returnVal.StatusCode = 401
-
+	if secret.String() != os.Getenv("SECRET") {
+            createdToken, err := ExampleNew([]byte(signingKey))
+            if err == nil {
+	        returnVal.Body = "bad token, here is a working jwt: " + createdToken
+	        returnVal.StatusCode = 401
+	    } else {
+	        returnVal.Body = "error creating token"
+	        returnVal.StatusCode = 500
+            }
 	} else {
-		fmt.Println("Hello Lambda")
-		// Do whatever you need to do here
-
-		returnVal.Body = "Completed request successfully " + param
+	    fmt.Println("Hello Lambda")
+            token, err := jwt.Parse(passedToken, func(token *jwt.Token) (interface{}, error) {
+                return []byte(signingKey), nil
+            })
+            if err == nil && token.Valid {
+                claims := token.Claims.(jwt.MapClaims)
+                // TODO test for presence of claim
+                returnVal.Body = claims["foo"].(string)
+            } else {
+                returnVal.Body = "jwt not valid or missing required claim"
+	        returnVal.StatusCode = 400
+            }
 	}
-
 	return returnVal, nil
 }
+
+func ExampleNew(mySigningKey []byte) (string, error) {
+    // Create the token
+    token := jwt.New(jwt.SigningMethodHS256)
+    // Set some claims
+    claims := make(jwt.MapClaims)
+    claims["foo"] = "bar"
+    claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
+    token.Claims = claims
+    // Sign and get the complete encoded token as a string
+    tokenString, err := token.SignedString(mySigningKey)
+    return tokenString, err
+}
+
